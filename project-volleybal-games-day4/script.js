2bd66cad113bc4bd5ae7b7f1c8a7c910fd31e858
Débora Gonçalves <deboracristinapereira123@gmail.com>
2021-03-25 22:56:11 -0300

    Links jogos

--- project-volleybal-games-day4/script.js
+++ project-volleybal-games-day4/script.js
@@ -1,14 +1,28 @@
-let games = new Array();
+let games = new Set();
+let gamesLink = new Set();
+
 let game;
+let gameLink;
 
 let trying = 5;
 
 for (let k = trying; k >= 1; k--) {
-    console.log(k);
-    game = prompt("Digite os " + k + " melhores jogos de Vôlei: ")
+
+    game = prompt("Digite os " + k + " melhores jogos de Vôlei: ");
+    gameLink = prompt("Digite o link dos " + k + " melhores jogos de Vôlei: ");
 
     if (game != null && game != undefined && !(game.length == 0)) {
-        games.push(game);
+
+        if (gamesLink != null && gamesLink != undefined && !(gamesLink.length == 0)) {
+
+            games.add(game)
+            gamesLink.add(gameLink);
+
+        } else {
+            alert("Digite um valor para o link do jogo de vôlei");
+            k++;
+        }
+        
     } else {
         alert("Digite um valor para o jogo de vôlei");
         k++;
@@ -17,6 +31,8 @@ for (let k = trying; k >= 1; k--) {
 }
 
 let listGames = document.querySelector(".list-games");
+
+// TO DO: Exibir links
 
 for (let game of games) {
     let itemList = document.createElement("li");
