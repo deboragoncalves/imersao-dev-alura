b2bf2ed7765882add1e9e8bf30b1637e663dc665
deboracristinapereira123@gmail.com <deboracristinapereira123@gmail.com>
2021-03-27 18:01:59 -0300

    Dividir em funcoes - Projeto Day 5

--- project-favorite-games-day5/script.js
+++ project-favorite-games-day5/script.js
@@ -2,23 +2,33 @@ function addGame() {
 
     let game = document.querySelector("#game");
     let linkGame = document.querySelector("#link-game");
-    let listGames = document.querySelector("#listGames");
 
     if (game.value != null && game.value != undefined && !(game.value.lenght == 0) && linkGame.value != null && linkGame.value != undefined && !(linkGame.value.lenght == 0)) {
-        let itemList = document.createElement("li");
-        let link = document.createElement("a");
+     
+        showList(game.value, linkGame.value);
 
-        link.href = linkGame.value;
-        link.target = "_blank";
-        link.textContent = linkGame.value;
-
-        itemList.textContent = game.value + "\n\n";
-        itemList.appendChild(link);
-
-        console.log(itemList)
-        listGames.appendChild(itemList);
     } else {
         alert("Informe o nome do jogo e o link.");
     }
 
+}
+
+function showList(nameGame, linkGame) {
+    let link = document.createElement("a");
+    let itemList = document.createElement("li");
+    let listGames = document.querySelector("#listGames");
+
+    // Link
+
+    link.href = linkGame;
+    link.target = "_blank";
+    link.textContent = linkGame;
+
+    // Nome
+
+    itemList.textContent = nameGame + "\n\n";
+
+    itemList.appendChild(link);
+
+    listGames.appendChild(itemList);
 }
